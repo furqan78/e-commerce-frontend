d26a22fe48816cab921cbbb1f4a4e0ff99a4bdac
furqan78 <furqangithub2@gmail.com>
2024-05-13 17:45:16 +0530

    update base url

--- src/app/constants/api-endpoints.js
+++ src/app/constants/api-endpoints.js
@@ -1,5 +1,5 @@
 export  const  apis = {
-    BASE_URL: 'https://ecommerce-backend-theta-dusky.vercel.app/',
+    BASE_URL: 'https://ecommerce-backend-theta-dusky.vercel.app',
     API_LOGIN_GET_TOKEN: "/auth/login",
     API_ADD_UPDATE_USER: "/user",
     API_USER_ACCOUNT_CREATION: "/auth",
